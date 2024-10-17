f63d7727998819131f3783b63ce869c75da16d5b
DanielBramlett <daniel.bramlett@Timothyministry.org>
2024-10-17 18:19:41 +0000

    Finshed

---
+++ CodingLessons/Homework/Function-Paras-and-Args.py
@@ -0,0 +1,36 @@
+def Math(Number_1,Number_2):
+    if isinstance(Number_1, (int, float)) and isinstance(Number_2, (int, float)):
+        result = Number_1 * Number_2
+        print(f'The result of {Number_1} multiplied by {Number_2} is {result}')
+    else:
+        print('Error: Both arguments must be numbers')
+
+Math(2,7)
+Math(3,2)
+Math(6,9)
+
+#End 1
+print()
+
+#def Movie(title, genre, year):
+#    print(f'Title : {title}\nGenre : {genre}\nYear : {year}')
+#Movie('Mutiny on the Bounty','Historical Adventure Drama','1962')
+
+#End 2
+print()
+
+Index = 0
+List = ['title', 'genre', 'year']
+Movie1 = {
+    'title': 'Mutiny on the Bounty',
+    'genre': 'Historical Adventure Drama',
+    'year': '1962'
+}
+
+def Movie2(movie_list):
+    Index = 0
+    for key in List:
+        print(f'{List[Index]}: {movie_list[key]}')
+        print('')
+        Index += 1
+Movie2(Movie1)
---
+++ CodingLessons/Homework/Functions.py
@@ -0,0 +1,8 @@
+def Name(Name):
+    print(f'My name is {Name}')
+
+Name('Daniel Bramlett')
+Name('Henry Avery')
+Name('Anne Bonney')
+
+#End 1
